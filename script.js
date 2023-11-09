87b8d51006d12b2867e89065fe045373b5dfd2fd
FulanoXD <110373735+FulanoXD@users.noreply.github.com>
2023-11-09 17:50:20 -0300

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-// Este código será executado após o carregamento completo do DOM
-document.addEventListener("DOMContentLoaded", function () {
-    // Adicione um ouvinte de evento ao elemento de classe "Menu_superior"
-    var menuItems = document.querySelectorAll(".Menu_superior");
-
-    menuItems.forEach(function (item) {
-        item.addEventListener("click", function () {
-            // Lidar com o clique no menu superior, se necessário
-            // Por exemplo, você pode rolar para a seção correspondente
-        });
+window.onload = function() {
+    // Definir a opacidade inicial de todos os elementos da página para 0
+    document.querySelectorAll("*").forEach(function(el) {
+      el.style.opacity = 0;
+      el.style.transform = "translate(-100%, 0)";
     });
-});
+  
+    // Definir um temporizador para alterar a opacidade e a posição dos elementos
+    setTimeout(function() {
+      document.querySelectorAll("*").forEach(function(el) {
+        el.style.opacity = 1;
+        el.style.transform = "translate(0, 0)";
+        el.style.transition = "transform 500ms ease-in";
+      });
+    }, 100);
+  };
+  
